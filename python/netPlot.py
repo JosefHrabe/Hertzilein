6b1c60e94e8014307f34e107786e1a9cb0f16bea
Josef Hrabe <etudes4db@gmail.com>
2022-01-10 21:46:11 +0100

    renames

--- python/netPlot.py
+++ python/netPlot.py
@@ -1,186 +1,159 @@
-import os, sys
-
-import matplotlib.pylab as plt
+import os
 import netBase as nb
+import compress_json as cj
+import matplotlib.pylab as plt
 import argparse
 
+cfg_showLegend=True
+cfg_ylim = ( 49.75,50.15 )
 imgSizeX=16
 imgSizeY=9
-cfg_showLegend=True
-cfg_showSamples=True
-cfg_normalize=True
-cfg_plot=True
-cfg_avgSample=[0.0]
-cfg_ylim= ( 0.0,0.0 )
-cfg_limit=False
+cfg_plot=False
 
 
-def mainFunc():
-    inList=[]
-    dList = os.listdir(nb.logDataPath)
-    dList.sort(reverse=True)
+def  makeMinuteData( xList , yList ):
+
+    data={}
+
+    for i,x in enumerate(xList) :
+        _x = '-'.join(x.split('-')[:-1])
+        if _x not in data:
+            data[_x]={ 'sum':0 , 'cnt':0}
+
+        data[_x]['sum']+=yList[i]
+        data[_x]['cnt']+=1
+
+    __x,__y=[],[]
+    for sample in data:
+        __x.append( sample+'-30' )
+        __y.append( data[sample]['sum'] / data[sample]['cnt'] )
+
+
+    for i,x in enumerate(__x):
+        __x[i] = nb.getDateObject(x)
+
+    pass
+    return __x , __y
+
+
+def plot():
 
 
     fig = plt.figure(num=None , figsize=(imgSizeX*0.6,imgSizeY*0.6) , dpi=150 )
-
-
+    dList = os.listdir( nb.slopeStrippedPath )
+    infiles=[]
     for d in dList:
-        dn = nb.logDataPath + d
-        if os.path.isdir( dn ):
-            if '/202' in dn:
-                fList = os.listdir( dn )
-                for f in fList:
-                    fn = dn + '/' + f
-                    if os.path.isfile ( fn ):
-                        inList.append( fn )
+        _dir = nb.slopeStrippedPath + d + '/'
+        fList = os.listdir( _dir )
 
+        for f in fList:
+            fn = _dir+f
+            infiles.append(fn)
 
-    inList.sort()
-    # for i in inList:    print(i)
+    infiles.sort()
 
-    if cfg_limit != 0:
-        while len(inList) > cfg_limit:
-            inList.pop(0)
+    sumData={ 'min':[] , 'max':[], 'avg':[]  , 't':[], 'to':[]  }
 
-
-    freqData=[]
-    time_str=[]
-
-    #fill raw data
-    for fn in inList:
-        resData = nb.loadDict( fn )
-
-        for i,r in enumerate(resData):
-            print('load {0}: {1:.2f}%'.format(fn.split('/')[-1] , ((i+1)/len(resData))*100) , end='\r')
-            time_str.append( r['t'] )
-            freqData.append( r['f'] )
-
+    for i,fn in enumerate(infiles):
+        print('Load: {0}'.format(fn) , end='\r')
+        data = nb.loadDict( fn )
+        sumData['min'].extend( data['min'] )
+        sumData['max'].extend( data['max'] )
+        sumData['avg'].extend( data['avg'] )
+        sumData['t'].extend( data['t'] )
     print()
-    #convert time data
-    timeData=[]
-    for t_s in time_str:
-        do = nb.getDateObject( t_s)
-        if do is not None:
-            timeData.append( do )
-    del time_str
 
-    print('Samples : {0}'.format(len(timeData)))
+    m_x , m_y = makeMinuteData( sumData['t'] , sumData['avg'])
+    plt.plot( m_x , m_y ,
+              label='Minute Average', marker='', linestyle='-',
+              color='#000000' , alpha = 0.9 , linewidth=0.7 , zorder=3)
 
-    mean_o= sum(freqData)/len(freqData)
 
-    if cfg_normalize:
-        offset = 50.0-mean_o
-
-        for i,f in enumerate(freqData): freqData[i] = f+offset
-
-    mean= sum(freqData)/len(freqData)
-
+    mean= sum(sumData['avg'])/len(sumData['avg'])
+    mean_50 = [ 50.0,50.0 ]
     mean_y = [ mean,mean ]
+    mean_x = [ nb.getDateObject( nb.getStamp(offset=-1e8) ) ,nb.getDateObject( nb.getStamp(offset=+1e8) ) ]
 
+    for i in range( len(sumData['t'])):
+        print( 'Convert: {0:.2f}%'.format( ((i+1)/len(sumData['t']))*100 ) , end='\r' )
+        sumData['to'].append( nb.getDateObject(sumData['t'][i]) )
 
-    mean_x = [ timeData[0] ,
-               #timeData[-1]
-               nb.getDateObject( nb.getStamp( offset=2*60*60 ) )
-               ]
+    plt.plot( sumData['to'] , sumData['avg'] ,
+              label='Mittelwert {0:.3f}Hz'.format(mean), marker='', linestyle='-',
+              color='#008000' , alpha = 0.4 , linewidth=0.7 , zorder=2)
 
-    flt = nb.Filter( )
 
     plt.plot(mean_x, mean_y, label='Mittelwert {0:.3f}Hz'.format(mean), marker='', linestyle=':',
              color='#008ff5' , linewidth=1 , zorder=2)
-    #plot data
-    if cfg_showSamples:
-        plt.plot(timeData, freqData, label='Original', marker=',', linestyle=':', color='#008ff540', zorder=1)
 
-    for avg in cfg_avgSample:
-        print('Make Trace : {0}'.format(avg))
-        outdata= flt.smooth(freqData, k=avg)
-        plt.plot( timeData ,  outdata ,
-                  label='Netzfrequenz ({0}Sa) - sym'.format(avg) , marker='' , linestyle='-' ,
-                  color='#000000' , alpha=0.7 , linewidth=0.7 ,  zorder=3)
-
-        # outdata= flt.smoothPast(freqData, k=avg*2)
-        # plt.plot( timeData ,  outdata ,
-        #           label='Netzfrequenz ({0}Sa) - past'.format(avg) , marker='' , linestyle='-' ,
-        #           color='#ff0000' , alpha=0.5 , linewidth=0.6 ,  zorder=3)
-
+    plt.plot(mean_x, mean_50, label='Mittelwert {0:.3f}Hz'.format(50), marker='', linestyle=':',
+             color='#ff0000' , linewidth=1 , zorder=2)
 
     if cfg_showLegend:
         #plot esctions
         #normal area
         dev = 0.02
-        dMin = [ mean-dev,mean-dev ]
-        dMax = [ mean+dev,mean+dev ]
-        plt.plot(mean_x, dMin, marker='', linestyle=':', color='#00aa00' , label='Normalbereich')
-        plt.plot(mean_x, dMax, marker='', linestyle=':', color='#00aa00')
+        mean_base = 50.0
+        dMin = [ mean_base-dev,mean_base-dev ]
+        dMax = [ mean_base+dev,mean_base+dev ]
+        plt.plot(mean_x, dMin, marker='', linestyle=':', linewidth=1 , color='#00aa00' , label='Normalbereich')
+        plt.plot(mean_x, dMax, marker='', linestyle=':', linewidth=1 , color='#00aa00')
         plt.fill_between( mean_x , dMin , dMax , color='#00a00040')
 
         #area with short distortion w/o prim. regulation
         dev = 0.8
-        dMin = [ mean-dev,mean-dev ]
-        dMax = [ mean+dev,mean+dev ]
-        plt.plot(mean_x, dMin, marker='', linestyle='-.', color='#00aa00' , label='Kurzfr. Störungen\nohne Regulierung')
-        plt.plot(mean_x, dMax, marker='', linestyle='-.', color='#00aa00')
+        dMin = [ mean_base-dev,mean_base-dev ]
+        dMax = [ mean_base+dev,mean_base+dev ]
+        plt.plot(mean_x, dMin, marker='', linestyle='-.', linewidth=1 , color='#00aa00' , label='Kurzfr. Störungen\nohne Regulierung')
+        plt.plot(mean_x, dMax, marker='', linestyle='-.', linewidth=1 , color='#00aa00')
         plt.fill_between( mean_x , dMin , dMax , color='#00a00020')
 
         # prim reg.
-        dMin = [ mean-0.2,mean-0.2 ]
-        dMax = [ mean-0.8,mean-0.8 ]
-        plt.plot(mean_x, dMin, marker='', linestyle='-', color='#606060' , alpha=0.5, label='Aktivierung Leistungsreserve\nt<10')
+        dMin = [ mean_base-0.2,mean_base-0.2 ]
+        dMax = [ mean_base-0.8,mean_base-0.8 ]
+        plt.plot(mean_x, dMin, marker='', linestyle='-', linewidth=1 , color='#606060' , alpha=0.5, label='Aktivierung Leistungsreserve\nt<10')
         plt.fill_between( mean_x , dMin , dMax , color='#ffff00' , alpha=0.1)
 
         # Abwurf Speicherpumpen
-        dMin = [ mean-0.8,mean-0.8 ]
-        dMax = [ mean-1.0,mean-1.0 ]
-        plt.plot(mean_x, dMin, marker='', linestyle='-', color='#606060' , alpha=0.6, label='Abwurf Speicherpumpen\nunverzögert')
+        dMin = [ mean_base-0.8,mean_base-0.8 ]
+        dMax = [ mean_base-1.0,mean_base-1.0 ]
+        plt.plot(mean_x, dMin, marker='', linestyle='-', linewidth=1 , color='#606060' , alpha=0.6, label='Abwurf Speicherpumpen\nunverzögert')
         plt.fill_between( mean_x , dMin , dMax , color='#ffff00' , alpha=0.2)
 
         # Lastabwurf Stufe 1
-        dMin = [ mean-1.0,mean-1.0 ]
-        dMax = [ mean-1.2,mean-1.2 ]
-        plt.plot(mean_x, dMin, marker='', linestyle='-', color='#ffff00' , alpha=0.7, label='Lastabwurf Stufe 1\nca. 12.5%')
+        dMin = [ mean_base-1.0,mean_base-1.0 ]
+        dMax = [ mean_base-1.2,mean_base-1.2 ]
+        plt.plot(mean_x, dMin, marker='', linestyle='-', linewidth=1 , color='#ffff00' , alpha=0.7, label='Lastabwurf Stufe 1\nca. 12.5%')
         plt.fill_between( mean_x , dMin , dMax , color='#ffff00' , alpha=0.3)
 
         # Lastabwurf Stufe 2
-        dMin = [ mean-1.2,mean-1.2 ]
-        dMax = [ mean-1.4,mean-1.4 ]
-        plt.plot(mean_x, dMin, marker='', linestyle='-', color='#ff8000' , alpha=0.7, label='Lastabwurf Stufe 2\nca. 25.0%')
+        dMin = [ mean_base-1.2,mean_base-1.2 ]
+        dMax = [ mean_base-1.4,mean_base-1.4 ]
+        plt.plot(mean_x, dMin, marker='', linestyle='-', linewidth=1 , color='#ff8000' , alpha=0.7, label='Lastabwurf Stufe 2\nca. 25.0%')
         plt.fill_between( mean_x , dMin , dMax , color='#ff8000' , alpha=0.4)
 
         # Lastabwurf Stufe 3
-        dMin = [ mean-1.4,mean-1.4 ]
-        dMax = [ mean-1.6,mean-1.6 ]
-        plt.plot(mean_x, dMin, marker='', linestyle='-', color='#ff4000' , alpha=0.7, label='Lastabwurf Stufe 3\nca. 37.5%')
+        dMin = [ mean_base-1.4,mean_base-1.4 ]
+        dMax = [ mean_base-1.6,mean_base-1.6 ]
+        plt.plot(mean_x, dMin, marker='', linestyle='-', linewidth=1 , color='#ff4000' , alpha=0.7, label='Lastabwurf Stufe 3\nca. 37.5%')
         plt.fill_between( mean_x , dMin , dMax , color='#ff4000' , alpha=0.5)
 
         # Lastabwurf Stufe 4
-        dMin = [ mean-1.6,mean-1.6 ]
-        dMax = [ mean-2.5,mean-2.5 ]
-        plt.plot(mean_x, dMin, marker='', linestyle='-', color='#ff0000' , alpha=0.7, label='Lastabwurf Stufe 4\nmind. 50%')
+        dMin = [ mean_base-1.6,mean_base-1.6 ]
+        dMax = [ mean_base-2.5,mean_base-2.5 ]
+        plt.plot(mean_x, dMin, marker='', linestyle='-', linewidth=1 , color='#ff0000' , alpha=0.7, label='Lastabwurf Stufe 4\nmind. 50%')
         plt.fill_between( mean_x , dMin , dMax , color='#ff0000' , alpha=0.6)
 
 
         # Blackout
-        dMin = [ mean-2.5,mean-2.5 ]
+        dMin = [ mean_base-2.5,mean_base-2.5 ]
         dMax = [ 0,0 ]
-        plt.plot(mean_x, dMin, marker='', linestyle='-', color='#000000' , alpha=1.0, label='Trennung Kraftwerke\nvon Netz')
+        plt.plot(mean_x, dMin, marker='', linestyle='-', linewidth=1 , color='#000000' , alpha=1.0, label='Trennung Kraftwerke\nvon Netz')
         plt.fill_between( mean_x , dMin , dMax , color='#000000' , alpha=0.6)
 
-
-
-    slpdata = nb.loadDict( nb.slopePath+'test.txt'  )
-    slpWfm = flt.calcArithmetics( slpdata)
-
-    plt.plot( slpWfm['t'] ,  slpWfm['avg'] ,
-              label='avg' , marker=',' , linestyle='-' ,
-              color='#6000a0' , alpha=0.7 , linewidth=0.5 ,  zorder=5)
-
-
     plt.grid()
-    if cfg_ylim == ( 0.0,0.0 ):
-        dev=0.022
-        plt.ylim(( mean-dev , mean+dev))
-    else:
-        plt.ylim( cfg_ylim )
+
+    plt.ylim( cfg_ylim )
 
     plt.xlim(( nb.getDateObject( nb.getStamp( -24*60*60)),
                nb.getDateObject( nb.getStamp( 2*60*60 ))))
@@ -188,11 +161,6 @@ def mainFunc():
                 ncol=6, mode="expand", borderaxespad=0. ,
                 fontsize=5)
 
-
-    # snap_cursor = nb.SnappingCursor(ax, line , timeData )
-    # fig.canvas.mpl_connect('motion_notify_event', snap_cursor.on_mouse_move)
-    # cursor = nb.Cursor(ax)
-    # fig.canvas.mpl_connect('motion_notify_event', cursor.on_mouse_move)
 
     fig.subplots_adjust(top=0.87)
     fig.subplots_adjust(bottom=0.05)
@@ -203,9 +171,6 @@ def mainFunc():
 
     title = 'Netzfrequenz'
 
-    if cfg_normalize:
-        title+=' - {0:.3f}Hz - Normiert'.format( mean_o )
-
     plt.title( title , size=9, y=1.09)
 
     if cfg_plot:
@@ -215,51 +180,5 @@ def mainFunc():
 
 
 if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('-hidelegend'  , dest='hidelegend', required=False , action='store_true',
-                        help='Hide legend')
-    parser.set_defaults(hidelegend=False)
-
-    parser.add_argument('-samples'  , dest='samples', required=False , action='store_true',
-                        help='Show samples')
-    parser.set_defaults(samples=False)
-
-    parser.add_argument('-norm'  , dest='norm', required=False , action='store_true',
-                        help='Normalize mean to 50Hz')
-    parser.set_defaults(norm=False)
-
-    parser.add_argument('-avgTrace'  , type=int, default=120   , nargs = '+'   , required=False ,
-                        help='120Sa, Average calc., samples BEFORE & AFTER current sample')
-
-    parser.add_argument('-plot'  , dest='plot', required=False , action='store_true',
-                        help='Plot output into file')
-    parser.set_defaults(plot=False)
-
-    parser.add_argument('-yLim', type=float, nargs=2 , default=0 , required=False  ,
-                        help='vertical default limit in diagram')
-
-    parser.add_argument('-limit'  , type=int, default=48  , required=False ,
-                        help='48h, number of hours to be displayed')
-
-
-
-
-    args = parser.parse_args()
-    cfg_showLegend  = not args.hidelegend
-    cfg_showSamples = args.samples
-    cfg_normalize   = args.norm
-    cfg_plot        = args.plot
-    cfg_limit       = args.limit
-
-    if type( args.avgTrace ) is list :
-        cfg_avgSample   = args.avgTrace
-    else:
-        cfg_avgSample = [args.avgTrace]
-
-    if type( args.yLim ) is list :
-        cfg_ylim = ( min(args.yLim) , max(args.yLim) )
-    mainFunc()
-
-
+    # cfg_plot=True
+    plot()
